25c5a09319fc34c858df3cf5654b45dc7f842e5a
johnhaviland <138923412+johnhaviland@users.noreply.github.com>
2023-11-16 11:56:20 -0700

    Update demoScene.cpp
    Implemented high score

--- demoScene.cpp
+++ demoScene.cpp
@@ -2,53 +2,54 @@
 #include <GL/glu.h>
 #include <cmath>
 #include <iostream>
+#include <chrono>
+#include <fstream>
 
-// Constants and variables from the second code
 const float PI = 3.14159265359;
 const int num_segments = 100;
 
-float rotationAngleX = 80.0f; // Rotation angle around the x-axis
-float rotationAngleY = 0.0f; // Rotation angle around the y-axis
+float rotationAngleX = 80.0f;
+float rotationAngleY = 0.0f;
 
-// Game variables
 float basketX = 0.0f;
 float basketSpeed = 0.02f;
 
 float ballX = 0.0f;
 float ballY = -0.9f;
-float ballSpeed = 0.05f;  // Adjusted for slower speed
-float ballArc = 1.5f;    // Adjusted to arch towards the side of the rim
+float ballSpeed = 0.05f;
+float ballArc = 1.5f;
 bool ballShot = false;
 
-int score = 0;
+float netX = 0.0f;
+float netY = 0.8f;
+float netWidth = 0.2f;
+float netHeight = 0.2f;
+bool ballInNet = false;
 
-// Window size
+int score = 0;
+int highScore = 0;
+
 const int windowWidth = 1900;
 const int windowHeight = 1080;
 
+std::chrono::time_point<std::chrono::steady_clock> startTime;
 
-// Function to handle key press
-// SPACE bar = shoot
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     if (key == GLFW_KEY_SPACE && action == GLFW_PRESS && !ballShot) {
         ballShot = true;
     }
 }
 
-// Function to update the game logic
 void updateGameLogic() {
-    // Update basket position
     basketX += basketSpeed;
     if (basketX >= 2.0f || basketX <= -2.0f) {
         basketSpeed = -basketSpeed;
     }
 
-    // Update ball position and trajectory when shot
     if (ballShot) {
         ballX += ballSpeed * cos(ballArc);
         ballY += ballSpeed * sin(ballArc);
 
-        // Check if the ball is in the basket
         if (fabs(ballX - basketX) < 0.1f && fabs(ballY - 1.0f) < 0.1f) {
             score++;
             ballShot = false;
@@ -56,16 +57,44 @@ void updateGameLogic() {
             ballY = -0.9f;
         }
 
-    // Reset ball if it goes past the screen or reaches the top
-    if (ballX > 2.0f || ballX < -2.0f || ballY > 2.0f || ballY < -2.0f) {
-        ballShot = false;
-        ballX = 0.0f;
-        ballY = -0.9f;
+        if (ballX > 2.0f || ballX < -2.0f || ballY > 2.0f || ballY < -2.0f) {
+            ballShot = false;
+            ballX = 0.0f;
+            ballY = -0.9f;
+        }
+    }
+
+    if (ballInNet) {
+        netY -= 0.01f;
+        if (netY < -1.0f) {
+            netY = 0.8f;
+            ballInNet = false;
+        }
+    }
+
+    // Implement the time tracking logic
+    static bool gameStarted = false;
+    if (!gameStarted && ballShot) {
+        startTime = std::chrono::steady_clock::now();
+        gameStarted = true;
+    }
+
+    if (gameStarted) {
+        auto currentTime = std::chrono::steady_clock::now();
+        auto elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(currentTime - startTime).count();
+        if (elapsedTime >= 60) {
+            glfwSetWindowShouldClose(glfwGetCurrentContext(), GLFW_TRUE);
+        }
+    }
+
+    if (ballShot) {
+        if (ballX >= netX - netWidth / 2 && ballX <= netX + netWidth / 2 &&
+            ballY <= netY && ballY >= netY - netHeight) {
+            ballInNet = true;
         }
     }
 }
 
-// Function to draw a 3D circle for the hoop
 void draw3DCircle(float radius, float lineWidth) {
     glLineWidth(lineWidth);
     glBegin(GL_LINE_LOOP);
@@ -79,15 +108,57 @@ void draw3DCircle(float radius, float lineWidth) {
     glEnd();
 }
 
+void drawNet() {
+    glColor3f(1.0f, 1.0f, 1.0f); // White color for the net
+    glLineWidth(2.0f);
+
+    glBegin(GL_LINES);
+    for (int i = 0; i < num_segments; ++i) {
+        float theta = 2.0f * PI * float(i) / float(num_segments);
+        float x1 = netX + netWidth / 2 * cosf(theta);
+        float y1 = netY - netHeight / 2 * sinf(theta);
+        float x2 = netX + netWidth / 2 * cosf(theta + 0.1f);
+        float y2 = netY - netHeight / 2 * sinf(theta + 0.1f);
+        glVertex2f(x1, y1);
+        glVertex2f(x2, y2);
+    }
+    glEnd();
+}
+
+void saveHighScore() {
+    std::ofstream file("highscore.txt");
+    if (file.is_open()) {
+        file << highScore;
+        file.close();
+    } else {
+        std::cerr << "Unable to save high score to file!" << std::endl;
+    }
+}
+
+void loadHighScore() {
+    std::ifstream file("highscore.txt");
+    if (file.is_open()) {
+        file >> highScore;
+        file.close();
+    } else {
+        std::cerr << "Unable to load high score from file! Initializing high score to zero." << std::endl;
+        highScore = 0;
+    }
+}
+
+void updateHighScore(int currentScore) {
+    if (currentScore > highScore) {
+        highScore = currentScore;
+        saveHighScore();
+    }
+}
 
 int main() {
-    // Initialize GLFW
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
 
-    // Create a windowed mode window and its OpenGL context
     GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "Basketball Flash Game", NULL, NULL);
     if (!window) {
         glfwTerminate();
@@ -95,13 +166,9 @@ int main() {
         return -1;
     }
 
-    // Make the window's context current
     glfwMakeContextCurrent(window);
-
-    // Set the key callback function
     glfwSetKeyCallback(window, key_callback);
 
-    // Set up viewport and projection
     glViewport(0, 0, windowWidth, windowHeight);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -109,58 +176,58 @@ int main() {
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    // Loop until the user closes the window
-    while (!glfwWindowShouldClose(window) && score < 5) {
-        // Update game logic
+    loadHighScore();
+
+    while (!glfwWindowShouldClose(window)) {
         updateGameLogic();
 
-        // Render scene
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glLoadIdentity();
         glTranslatef(0.0f, 0.0f, -5.0f);
 
-        // Draw the hoop
         glPushMatrix();
-        glTranslatef(basketX, 1.0f, 0.0f); // Adjust to place the hoop correctly
+        glTranslatef(basketX, 1.0f, 0.0f);
         glRotatef(rotationAngleX, 1.0f, 0.0f, 0.0f);
         draw3DCircle(0.2f, 2.0f);
         glPopMatrix();
 
-        // Draw the backboard
         glBegin(GL_QUADS);
-        glColor3f(0.0f, 0.0f, 1.0f); // Blue color for the backboard
+        glColor3f(0.0f, 0.0f, 1.0f);
         glVertex2f(basketX - 0.5f, 1.0f);
         glVertex2f(basketX + 0.5f, 1.0f);
         glVertex2f(basketX + 0.5f, 1.1f);
         glVertex2f(basketX - 0.5f, 1.1f);
         glEnd();
 
-        // Draw the rim
         glBegin(GL_QUADS);
-        glColor3f(1.0f, 0.0f, 0.0f); // Red color for the rim
+        glColor3f(1.0f, 0.0f, 0.0f);
         glVertex2f(basketX - 0.1f, 0.8f);
         glVertex2f(basketX + 0.1f, 0.8f);
         glVertex2f(basketX + 0.1f, 0.82f);
         glVertex2f(basketX - 0.1f, 0.82f);
         glEnd();
 
-        // Draw the ball
         glBegin(GL_QUADS);
-        glColor3f(1.0f, 0.647f, 0.0f); // Orange color for the ball
+        glColor3f(1.0f, 0.647f, 0.0f);
         glVertex2f(ballX - 0.05f, ballY - 0.05f);
         glVertex2f(ballX + 0.05f, ballY - 0.05f);
         glVertex2f(ballX + 0.05f, ballY + 0.05f);
         glVertex2f(ballX - 0.05f, ballY + 0.05f);
         glEnd();
 
-        // Swap front and back buffers
+        // drawNet();
+
         glfwSwapBuffers(window);
 
-        // Poll for and process events
         glfwPollEvents();
     }
 
-    // Terminate GLFW
+    updateHighScore(score);
+
+    // Display score and high score after the window closes
+    std::cout << "Your score: " << score << std::endl;
+    std::cout << "High score: " << highScore << std::endl;
+
     glfwTerminate();
     return 0;
 }
